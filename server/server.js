27077608916d8f52953ab2b31304a6d41401c01e
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-07-12 01:48:03 +0500

    backend setup

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
-const express = require('express')
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require('morgan')
+const cors = require('cors')
+dotenv.config();
 
-const app = express()
+const app = express();
 
-
+//middlewares
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
 
 //routes
-app.get('/test' , (req,res) =>{
 
-
-    res.status(200).send("halo9")
-})
-
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Node server running in ${process.env.DEV_MODE} Mode on port ${port}`.bgBlue.white );
+});
